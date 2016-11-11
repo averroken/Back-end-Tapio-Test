c557235367de626ed4a8b659e2ef89117648577c
Brian-Mas <brianmasschaele@icloud.com>
2016-11-11 23:59:04 +0100

    v0.3.2 | facebook registration/login added

--- app.js
+++ app.js
@@ -10,27 +10,70 @@ var config = require('./oauth.js');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
+// mongo config
+var mongo = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TestV3'
+mongoose.connect(mongo);
+
+// mongo model
+// var Model_Name = require('add_your_models_here');
+var User = mongoose.model('User', {
+    oauthID: Number,
+    name: String,
+    created: Date
 });
 
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
+
+passport.serializeUser(function(user, done) {
+    console.log('serializeUser: ' + user._id);
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+        console.log(user);
+        if (!err) {
+            done(null,user);
+        }else {
+            done(err, null);
+        }
+    })
 })
 
 passport.use(new FacebookStrategy({
-    clientID: '100697627081768',
-    clientSecret: '7db636cbc4fc5a51af01b6160fdc4efa',
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret,
+        callbackURL: config.facebook.callbackUrl
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOne({oauthID: profile.id}, function (err, user) {
+            if(err){
+                console.log(err);
+            }
+            if(!err && user !== null){
+                done(null, user);
+            }else {
+                user = new User({
+                    oauthID: profile.id,
+                    name: profile.dispalyName,
+                    created: Date.now()
+                });
+                user.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    }else {
+                        console.log("saving user");
+                        done(null, user);
+                    }
+                })
+            }
+        })
+    }
 ));
-
 
 
 // global config
@@ -38,35 +81,77 @@ var app = express();
 app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(bodyParser());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(methodOverride());
+app.use(session({
+    secret: 'ilovechocolate',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // env config
-app.configure('development', function() {
-    app.use(express.errorHandler({
+var enviroment = process.env.NODE_ENV || 'development';
+if (enviroment == 'development') {
+    app.use(errorHandler({
         dumpExceptions: true,
         showStack: true
-    }));
-});
-
-app.configure('production', function() {
-    app.use(express.errorHandler());
-});
-
-// mongo config
-var MONGOLAB_URI = "add_your_mongolab_uri_here"
-var mongo = process.env.MONGOLAB_URI || 'mongodb://localhost/node-bootstrap3-template'
-mongoose.connect(mongo);
-
-// mongo model
-// var Model_Name = require('add_your_models_here');
+    }))
+} else if (enviroment == 'production') {
+    app.use(errorHandler());
+}
 
 // routes
 app.get('/', routes.index);
 app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res) {
+    User.findById(req.session.passport.user, function (err, user) {
+        if (err) {
+            console.log(err);
+        }else {
+            res.render('account', {user: user});
+        }
+    })
+});
+
+app.get('/', function(req, res) {
+    res.render('login', {
+        user: req.user
+    });
+});
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook'),
+    function(req, res) {});
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        failureRedirect: '/'
+    }),
+    function(req, res) {
+        res.redirect('/account');
+});
+
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+})
 
 // run server
 app.listen(app.get('port'), function() {
     console.log('\nExpress server listening on port ' + app.get('port'));
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
