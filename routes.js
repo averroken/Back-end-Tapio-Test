955bff42a433dfc01d64df397a7c1fd3f6048c2d
Brian-Mas <brianmasschaele@icloud.com>
2016-11-14 10:38:14 +0100

    v0.4.0 | Working (locally tested)
    Models/account: added token filed (with default ‘null’)
    
    Routes:
     - added jsonwebtoken require
     - added isAuthenticated function
     - added username taken message
     - added /authenticate route (generates token, displays it and saves to
    db)

--- routes.js
+++ routes.js
@@ -1,13 +1,24 @@
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 var Account = require('./models/account');
 
 module.exports = function(app) {
+    function isAuthenticated(req, res, next) {
+        if (req.user) {
+            return next();
+        }
+
+        res.render('login', {
+            info: "Please login"
+        })
+    }
+
     app.get('/', function(req, res) {
         console.log('user: ' + req.user);
-            res.render('index', {
-                user: req.user
-            });
+        res.render('index', {
+            user: req.user
+        });
     });
 
     app.get('/register', function(req, res) {
@@ -22,29 +33,51 @@ module.exports = function(app) {
             function(err, account) {
                 if (err) {
                     console.log("error: " + err);
-                    return res.render('register', {info: "Sorry. That username is already taken"})
+                    return res.render('register', {
+                        info: "Sorry. That username is already taken"
+                    })
                 }
 
-                passport.authenticate('local')(req,res,function () {
+                passport.authenticate('local')(req, res, function() {
                     res.redirect('/');
                 });
             });
     });
 
-    app.get('/login', function (req,res) {
-        res.render('login', {user: req.user});
+    app.get('/login', function(req, res) {
+        res.render('login', {
+            user: req.user
+        });
     });
 
-    app.post('/login', passport.authenticate('local'), function (req, res) {
+    app.post('/login', passport.authenticate('local'), function(req, res) {
         res.redirect('/');
     })
 
-    app.get('/logout', function (req, res) {
+    app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
 
-    app.get('/ping', function (req, res) {
+    app.get('/ping', isAuthenticated, function(req, res) {
         res.send("pong!", 200);
     })
-};
+
+    //route to authenticate users
+    app.get('/authenticate', isAuthenticated, function(req, res) {
+        var user = new Account(req.user);
+        var token = jwt.sign(user, 'ilovechocolate', {
+            expiresIn: 1440
+        });
+        user.token = token;
+
+        res.json({
+            user: req.user.username,
+            success: true,
+            message: 'Enjoy your token',
+            token: token
+        });
+
+        user.save();
+    });
+}
