41aad51bcde328e49a1e8e0c808e05398fc8a8bd
Brian-Mas <brianmasschaele@icloud.com>
2016-11-20 21:41:27 +0100

    Added port to callback urls

--- oauthCredentialsHeroku.js
+++ oauthCredentialsHeroku.js
@@ -2,11 +2,11 @@ module.exports = {
     facebook:{
         clientID: process.env.FACEBOOK_CLIENT_ID,
         clientSecret:process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: 'https://back-end-tapio-test.herokuapp.com/auth/facebook/callback'
+        callbackURL: 'https://back-end-tapio-test.herokuapp.com:'+ process.env.PORT+'/auth/facebook/callback'
     },
     google:{
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
+        callbackURL: 'https://back-end-tapio-test.herokuapp.com:'+ process.env.PORT+'/auth/google/callback'
     }
 };
