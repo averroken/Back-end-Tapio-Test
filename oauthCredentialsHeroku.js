1023aa61e39e92b45a9851c68a5d14fb510db3d0
Brian-Mas <brianmasschaele@icloud.com>
2016-11-20 22:25:26 +0100

    Deleted ports from social callbacks

--- oauthCredentialsHeroku.js
+++ oauthCredentialsHeroku.js
@@ -2,11 +2,11 @@ module.exports = {
     facebook:{
         clientID: process.env.FACEBOOK_CLIENT_ID,
         clientSecret:process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: 'https://back-end-tapio-test.herokuapp.com:'+ process.env.PORT+'/auth/facebook/callback'
+        callbackURL: 'https://back-end-tapio-test.herokuapp.com/auth/facebook/callback'
     },
     google:{
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://back-end-tapio-test.herokuapp.com:'+ process.env.PORT+'/auth/google/callback'
+        callbackURL: 'https://back-end-tapio-test.herokuapp.com/auth/google/callback'
     }
 };
