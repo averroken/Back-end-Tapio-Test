636cba40676061e1c25d02e9b26641e1ef0349b6
averroken <averroken@outlook.com>
2016-11-21 12:01:51 +0100

    commented social logins (for heroku)

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const FacebookStrategy = require('passport-facebook');
-const GoogleStrategy = require('passport-google-oauth2');
-var config = require('./oauthCredentials.js')
+// const FacebookStrategy = require('passport-facebook');
+// const GoogleStrategy = require('passport-google-oauth2');
+// var config = require('./oauthCredentials.js')
 var Account = require('./models/account');
 var express = require('express');
 var testRoutes = express.Router();
@@ -116,106 +116,106 @@ module.exports = function(app) {
         user.save();
     });
 
-    // Passport Strategies
-    passport.use(new FacebookStrategy({
-            clientID: config.facebook.clientID,
-            clientSecret: config.facebook.clientSecret,
-            callbackURL: config.facebook.callbackURL
-        },
-        function(accessToken, refreshToken, profile, done) {
-            Account.findOne({
-                socialLoginId: profile.id
-            }, function(err, user) {
-                if (err) {
-                    console.log(err);
-                }
-                if (!err && user !== null) {
-                    done(null, user);
-                } else {
-                    account = new Account({
-                        username: "" + profile.id,
-                        socialUsername: profile.displayName,
-                        socialLoginId: profile.id,
-                        created: Date.now(),
-                        authenticationMethod: "Facebook",
-                        facebokToken: accessToken
-                    });
-                    account.save(function(err) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("saving user");
-                            done(null, user);
-                        }
-                    })
-                }
-            })
-        }
-    ));
-
-    passport.use(new GoogleStrategy({
-            clientID: config.google.clientID,
-            clientSecret: config.google.clientSecret,
-            callbackURL: config.google.callbackURL,
-            passReqToCallback: true
-        },
-        function(request, accessToken, refreshToken, profile, done) {
-            Account.findOne({
-                socialLoginId: profile.id
-            }, function(err, user) {
-                if (err) {
-                    console.log(err);
-                }
-                if (!err && user !== null) {
-                    done(null, user);
-                } else {
-                    account = new Account({
-                        username: "" + profile.id,
-                        socialUsername: profile.displayName,
-                        socialLoginId: profile.id,
-                        created: Date.now(),
-                        authenticationMethod: "Google"
-                    });
-                    account.save(function(err) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("saving user");
-                            done(null, user);
-                        }
-                    })
-                }
-            })
-        }
-    ));
-
-    app.get('/auth/facebook',
-        passport.authenticate('facebook'),
-        function(req, res) {});
-
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            failureRedirect: '/'
-        }),
-        function(req, res) {
-            res.redirect('/');
-        });
-
-    app.get('/auth/google',
-        passport.authenticate('google', {
-            scope: [
-                'https://www.googleapis.com/auth/plus.login'
-            ]
-        })
-    );
-
-    app.get('/auth/google/callback',
-        passport.authenticate('google', {
-            failureRedirect: '/'
-        }),
-        function(req, res) {
-            res.redirect('/');
-        });
+    // // Passport Strategies
+    // passport.use(new FacebookStrategy({
+    //         clientID: config.facebook.clientID,
+    //         clientSecret: config.facebook.clientSecret,
+    //         callbackURL: config.facebook.callbackURL
+    //     },
+    //     function(accessToken, refreshToken, profile, done) {
+    //         Account.findOne({
+    //             socialLoginId: profile.id
+    //         }, function(err, user) {
+    //             if (err) {
+    //                 console.log(err);
+    //             }
+    //             if (!err && user !== null) {
+    //                 done(null, user);
+    //             } else {
+    //                 account = new Account({
+    //                     username: "" + profile.id,
+    //                     socialUsername: profile.displayName,
+    //                     socialLoginId: profile.id,
+    //                     created: Date.now(),
+    //                     authenticationMethod: "Facebook",
+    //                     facebokToken: accessToken
+    //                 });
+    //                 account.save(function(err) {
+    //                     if (err) {
+    //                         console.log(err);
+    //                     } else {
+    //                         console.log("saving user");
+    //                         done(null, user);
+    //                     }
+    //                 })
+    //             }
+    //         })
+    //     }
+    // ));
+    //
+    // passport.use(new GoogleStrategy({
+    //         clientID: config.google.clientID,
+    //         clientSecret: config.google.clientSecret,
+    //         callbackURL: config.google.callbackURL,
+    //         passReqToCallback: true
+    //     },
+    //     function(request, accessToken, refreshToken, profile, done) {
+    //         Account.findOne({
+    //             socialLoginId: profile.id
+    //         }, function(err, user) {
+    //             if (err) {
+    //                 console.log(err);
+    //             }
+    //             if (!err && user !== null) {
+    //                 done(null, user);
+    //             } else {
+    //                 account = new Account({
+    //                     username: "" + profile.id,
+    //                     socialUsername: profile.displayName,
+    //                     socialLoginId: profile.id,
+    //                     created: Date.now(),
+    //                     authenticationMethod: "Google"
+    //                 });
+    //                 account.save(function(err) {
+    //                     if (err) {
+    //                         console.log(err);
+    //                     } else {
+    //                         console.log("saving user");
+    //                         done(null, user);
+    //                     }
+    //                 })
+    //             }
+    //         })
+    //     }
+    // ));
+    //
+    // app.get('/auth/facebook',
+    //     passport.authenticate('facebook'),
+    //     function(req, res) {});
+    //
+    // app.get('/auth/facebook/callback',
+    //     passport.authenticate('facebook', {
+    //         failureRedirect: '/'
+    //     }),
+    //     function(req, res) {
+    //         res.redirect('/');
+    //     });
+    //
+    // app.get('/auth/google',
+    //     passport.authenticate('google', {
+    //         scope: [
+    //             'https://www.googleapis.com/auth/plus.login'
+    //         ]
+    //     })
+    // );
+    //
+    // app.get('/auth/google/callback',
+    //     passport.authenticate('google', {
+    //         failureRedirect: '/'
+    //     }),
+    //     function(req, res) {
+    //         res.redirect('/');
+    //     });
 
 
     testRoutes.use(function(req, res, next) {
