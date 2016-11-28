fb17ce1fd130c5de8b63be7419c2d373ee6b3156
averroken <averroken@outlook.com>
2016-11-28 12:05:42 +0100

    Added route to like landmark
    see the route at the bottom (patch) byID  + you get a friendly message
    back

--- Routes/landmarkRoutes.js
+++ Routes/landmarkRoutes.js
@@ -71,22 +71,39 @@ var routes = function (Landmark) {
             });
         });
 
-    landmarkRouter.use('/:landmarkId', function (req, res, next) {
-        Landmark.findById(req.params.landmarkId, function (err, landmark) {
-            if (err)
+    landmarkRouter.use('/:landmarkId', function(req,res,next){
+        Landmark.findById(req.params.landmarkId, function(err,landmark){
+            if(err)
                 res.status(500).send(err);
-            else if (landmark) {
+            else if(landmark)
+            {
                 req.landmark = landmark;
                 next();
             }
-            else {
+            else
+            {
                 res.status(404).send('no landmark found');
             }
         });
     });
+
     landmarkRouter.route('/:landmarkId')
         .get(function (req, res) {
             res.json(req.landmark);
+        })
+        .patch(function(req,res){
+            if(req.body._id)
+                delete req.body._id;
+
+            req.landmark.Likes = req.landmark.Likes+1;
+
+            req.landmark.save(function(err){
+                if(err)
+                    return res.status(406).send("You can't like this landmark !!!");
+                else{
+                    return res.status(201).send("Congratulations, You are the " + req.landmark.Likes.toString() + "'ste liker");
+                }
+            });
         });
 
     return landmarkRouter;
