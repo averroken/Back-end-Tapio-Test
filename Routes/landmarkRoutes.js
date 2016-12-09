45edb2e882ecaf87069770340d88c888e114df70
Brian-Mas <brianmasschaele@icloud.com>
2016-12-09 23:40:01 +0100

    Changed json response for add Landmark
    Both success and error (of POST landmark) now return a json object conatining an anwser (succes/no success) and message (landmark id / errors)
    
    Also beatified file

--- Routes/landmarkRoutes.js
+++ Routes/landmarkRoutes.js
@@ -1,142 +1,162 @@
 var express = require('express');
 
-var routes = function (Landmark) {
+var routes = function(Landmark) {
     var landmarkRouter = express.Router();
     landmarkRouter.route('/')
-        .post(function (req, res) {
+        .post(function(req, res) {
             var landmark = new Landmark(req.body);
 
-            landmark.save(function (err) {
-                if(err){
-                    return res.status(406).send("Het posten van de landmark is mislukt");
+            landmark.save(function(err) {
+                if (err) {
+                    var test = err;
+                    var json = {
+                        "awnser": "Failed to create json",
+                        "message": test.errors
+                    };
+                    return res.status(406).json(json);
                 } else {
-                    res.status(201).send("Successfully created landmark");
+                    var json = {
+                        "awnser": "Successfully created landmark",
+                        "message": {
+                          "id" : landmark._id 
+                        }
+                    };
+                    res.status(201).json(json);
                 }
             });
         })
-        .get(function (req, res) {
+        .get(function(req, res) {
 
             var query = {};
 
-            Landmark.find(query, function (err, landmarks) {
+            Landmark.find(query, function(err, landmarks) {
                 if (err)
                     res.status(500).send(err);
                 else
-                    res.json({"landmarks" : landmarks});
+                    res.json({
+                        "landmarks": landmarks
+                    });
             });
         });
 
     landmarkRouter.route('/test')
-        .get(function (req, res) {
-            Landmark.find({}, {"Long": 1, "Lat": 1}, function (err, landmarks) {
+        .get(function(req, res) {
+            Landmark.find({}, {
+                "Long": 1,
+                "Lat": 1
+            }, function(err, landmarks) {
                 if (err)
                     res.status(500).send(err);
                 else
-                    res.json({"landmarks" : landmarks});
+                    res.json({
+                        "landmarks": landmarks
+                    });
             });
         });
 
     landmarkRouter.route('/filterlocatie')
-        .get(function (req, res) {
+        .get(function(req, res) {
             var query = {};
 
-            if(req.query.Country)
-            {
-                if(req.query.Country) query.Country = req.query.Country;
+            if (req.query.Country) {
+                if (req.query.Country) query.Country = req.query.Country;
             }
 
-            Landmark.find(query, function (err, landmarks) {
+            Landmark.find(query, function(err, landmarks) {
                 if (err)
                     res.status(500).send(err);
                 else
-                    res.json({"landmarks" : landmarks});
+                    res.json({
+                        "landmarks": landmarks
+                    });
             });
         });
 
     landmarkRouter.route('/filterlocatieshort')
-        .get(function (req, res) {
+        .get(function(req, res) {
 
             var query = {};
 
-            if(req.query.Country)
-            {
-                if(req.query.Country) query.Country = req.query.Country;
+            if (req.query.Country) {
+                if (req.query.Country) query.Country = req.query.Country;
             }
 
-            Landmark.find(query, {"Long": 1, "Lat": 1}, function (err, landmarks) {
+            Landmark.find(query, {
+                "Long": 1,
+                "Lat": 1
+            }, function(err, landmarks) {
                 if (err)
                     res.status(500).send(err);
                 else
-                    res.json({"landmarks" : landmarks});
+                    res.json({
+                        "landmarks": landmarks
+                    });
             });
         });
 
-    landmarkRouter.use('/:landmarkId', function(req,res,next){
-        Landmark.findById(req.params.landmarkId, function(err,landmark){
-            if(err)
+    landmarkRouter.use('/:landmarkId', function(req, res, next) {
+        Landmark.findById(req.params.landmarkId, function(err, landmark) {
+            if (err)
                 res.status(500).send(err);
-            else if(landmark)
-            {
+            else if (landmark) {
                 req.landmark = landmark;
                 next();
-            }
-            else
-            {
+            } else {
                 res.status(404).send('no landmark found');
             }
         });
     });
 
     landmarkRouter.route('/:landmarkId/addlike')
-        .patch(function(req,res){
-            if(req.body._id)
+        .patch(function(req, res) {
+            if (req.body._id)
                 delete req.body._id;
 
-            req.landmark.Likes = req.landmark.Likes+1;
+            req.landmark.Likes = req.landmark.Likes + 1;
 
-            req.landmark.save(function(err){
-                if(err)
+            req.landmark.save(function(err) {
+                if (err)
                     return res.status(406).send("You can't like this landmark !!!");
-                else{
+                else {
                     return res.status(201).send("Congratulations, you are liker number: " + req.landmark.Likes.toString());
                 }
             });
         });
 
     landmarkRouter.route('/:landmarkId/deletelike')
-        .patch(function(req,res){
-            if(req.body._id)
+        .patch(function(req, res) {
+            if (req.body._id)
                 delete req.body._id;
 
-            req.landmark.Likes = req.landmark.Likes-1;
+            req.landmark.Likes = req.landmark.Likes - 1;
 
-            req.landmark.save(function(err){
-                if(err)
+            req.landmark.save(function(err) {
+                if (err)
                     return res.status(406).send("You can't like this landmark !!!");
-                else{
+                else {
                     return res.status(201).send("Congratulations, you are liker number: " + req.landmark.Likes.toString());
                 }
             });
         });
 
     landmarkRouter.route('/:landmarkId')
-        .get(function (req, res) {
+        .get(function(req, res) {
             res.json(req.landmark);
         });
-        /*.patch(function(req,res){
-            if(req.body._id)
-                delete req.body._id;
+    /*.patch(function(req,res){
+        if(req.body._id)
+            delete req.body._id;
 
-            req.landmark.Likes = req.landmark.Likes+1;
+        req.landmark.Likes = req.landmark.Likes+1;
 
-            req.landmark.save(function(err){
-                if(err)
-                    return res.status(406).send("You can't like this landmark !!!");
-                else{
-                    return res.status(201).send("Congratulations, you are liker number: " + req.landmark.Likes.toString());
-                }
-            });
-        });*/
+        req.landmark.save(function(err){
+            if(err)
+                return res.status(406).send("You can't like this landmark !!!");
+            else{
+                return res.status(201).send("Congratulations, you are liker number: " + req.landmark.Likes.toString());
+            }
+        });
+    });*/
     return landmarkRouter;
 };
 
