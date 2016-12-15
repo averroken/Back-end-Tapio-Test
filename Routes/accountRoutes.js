18d7de3148a6fb258a2a752a2d6adeba35402d84
averroken <averroken@outlook.com>
2016-12-15 20:50:27 +0100

    favourites account fixed

--- Routes/accountRoutes.js
+++ Routes/accountRoutes.js
@@ -89,38 +89,74 @@ var routes = function (Account) {
         /* Example API CALL */
         //localhost:1337/api/account/584fc0f39efc92183c26e857/addFavouriteLandmark?landmarkid=98745632222
         .patch(function(req, res) {
-            var landmarkid = req.query.landmarkid;
-            if(landmarkid){
-                    req.account.favourites.push({"landmarkID": landmarkid});
-                    console.log(req.account.favourites);
-                    req.account.save(function(err) {
-                        if (err) {
-                            //throw err;
-                            return res.status(406).send("Adding ID to account is failed!");
-                        }
-                    });
-                return res.status(201).send("Update succeeded: " + req.query.landmarkid);
-            } else {
-                res.status(406).send("I need the ID of the landmark you liked.");
-            }
+            Account.findById(req.params.accountId, function(err,account){
+                if(err)
+                    res.status(500).send(err);
+                else if(account)
+                {
+                    req.account = account;
+                    var landmarkid = req.query.landmarkid;
+                    if(landmarkid){
+                        req.account.favourites.push({"landmarkID": landmarkid});
+                        //console.log(req.account.favourites);
+                        req.account.save(function(err) {
+                            if (err) {
+                                //throw err;
+                                return res.status(406).send("Adding ID to account is failed!");
+                            }
+                        });
+                        return res.status(201).send("Update succeeded: " + req.query.landmarkid);
+                    } else {
+                        res.status(406).send("I need the ID of the landmark you liked.");
+                    }
+                }
+                else
+                {
+                    res.status(404).send('no account found');
+                }
+            });
         });
 
     accountRouter.route('/:accountId/removeFavouriteLandmark')
         .patch(function(req, res) {
-            var landmarkid = req.query.landmarkid;
-            if(landmarkid){
-                req.account.favourites.splice({landmarkID: landmarkid});
-                console.log(req.account.favourites);
-                req.account.save(function(err) {
-                    if (err) {
-                        //throw err;
-                        return res.status(406).send("Adding ID to account is failed!");
+            Account.findById(req.params.accountId, function(err,account){
+                if(err)
+                    res.status(500).send(err);
+                else if(account)
+                {
+                    req.account = account;
+                    var landmarkid = req.query.landmarkid;
+                    if(landmarkid){
+                        var favourites = account.favourites;
+                        var newfavorites = [];
+                        //console.log(landmarkid);
+                        for(var i = 0;i<favourites.length;i++){
+                            var favorite = favourites[i];
+                            //console.log(favorite.landmarkID);
+                            if(favorite.landmarkID == landmarkid){
+                                //console.log(i + ":" + favorite);
+                            } else {
+                                newfavorites.push(favorite);
+                            }
+                        }
+                        //console.log(newfavorites);
+                        account.favourites = newfavorites;
+                        account.save(function(err) {
+                            if (err) {
+                                //throw err;
+                                return res.status(406).send("Adding ID to account is failed!");
+                            }
+                        });
+                        return res.status(201).send("Update succeeded: " + req.query.landmarkid);
+                    } else {
+                        res.status(406).send("I need the ID of the landmark you liked.");
                     }
-                });
-                return res.status(201).send("Update succeeded: " + req.query.landmarkid);
-            } else {
-                res.status(406).send("I need the ID of the landmark you liked.");
-            }
+                }
+                else
+                {
+                    res.status(404).send('no account found');
+                }
+            });
         });
 
     return accountRouter;
