9f8c283bd953602ee5c2be2e950564e56c8e6ddf
DarthSwedo <matthieu19@msn.com>
2016-12-16 22:02:28 +0100

    Added upload route fix

--- routes/landmarkRoutes.js
+++ routes/landmarkRoutes.js
@@ -1,61 +1,12 @@
 var express = require('express');
 var wc = require('which-country');
 //Image upload
-var fs = require('fs');
-var Landmark = require('../models/landmarkModel');
-var multer = require('multer');
-var upload = multer(
-    {
-        limits: {
-            fieldNameSize: 999999999,
-            fieldSize: 999999999
-        },
-        dest: 'uploads/' }
-);
-app.post('/upload', upload.any(), function (req, res) {
-    var landmarkId = req.params.landmarkId;
-    if(landmarkId) {
-        Landmark.findOne({
-            _id: landmarkId
-        }, function (err, landmark) {
-
-            console.log("file: " + req.files);
-
-            var tmp_path = req.files[0].path;
-
-            var target_path = 'uploads/' + req.files[0].originalname;
-
-            var src = fs.createReadStream(tmp_path);
-            var dest = fs.createWriteStream(target_path);
-            src.pipe(dest);
-            src.on('end', function () {
-                res.send("ok: " + target_path);
-            });
-            src.on('error', function (err) {
-                res.send({error: "upload failed"});
-            });
-            landmark.Image = target_path;
-            res.status(201).send(landmark);
-        });
-    } else {
-        res.status(500);
-        console.log("There is no landmarkId given");
-    }
-});
 
 var routes = function (Landmark) {
     var landmarkRouter = express.Router();
     landmarkRouter.route('/')
-        .post(upload.any(),function (req, res) {
-            var tmp_path = req.files[0].path;
-
-            var target_path = 'uploads/' + req.files[0].originalname;
+        .post(function (req, res) {
             var landmark = new Landmark(req.body);
-            console.log('File name: ' + target_path);
-            console.log('Landmark: ' + landmark.Name);
-            var src = fs.createReadStream(tmp_path);
-            var dest = fs.createWriteStream(target_path);
-            src.pipe(dest);
             var lon = landmark.Long;
             var lat = landmark.Lat;
             landmark.Image = target_path;
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,7 @@
-var fs = require('fs');
-
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
+var Landmark = require('../models/landmarkModel');
 var multer = require('multer');
 var upload = multer(
     {
@@ -14,27 +13,35 @@ var upload = multer(
 );
 module.exports = function (app) {
     app.post('/upload', upload.any(), function (req, res) {
+        var landmarkId = req.params.landmarkId;
+        console.log('LandmarkId given is: ' + landmarkId);
+        if(landmarkId) {
+            Landmark.findOne({
+                _id: landmarkId
+            }, function (err, landmark) {
+                console.log('Landmark found is: ' + landmark.Name);
+                console.log("file: " + req.files);
 
-        console.log("file: " + req.files);
+                var tmp_path = req.files[0].path;
 
-        var tmp_path = req.files[0].path;
+                var target_path = 'uploads/' + req.files[0].originalname;
 
-        var target_path = 'uploads/' + req.files[0].originalname;
-
-        var src = fs.createReadStream(tmp_path);
-        var dest = fs.createWriteStream(target_path);
-        src.pipe(dest);
-        src.on('end', function () {
-            res.send("ok: " + target_path);
-        });
-        src.on('error', function (err) {
-            res.send({error: "upload failed"});
-        });
-    });
-
-    app.get('/info', function (req, res) {
-        console.log(__dirname);
-        res.send("image upload server: post /upload");
+                var src = fs.createReadStream(tmp_path);
+                var dest = fs.createWriteStream(target_path);
+                src.pipe(dest);
+                src.on('end', function () {
+                    res.send("ok: " + target_path);
+                });
+                src.on('error', function (err) {
+                    res.send({error: "upload failed"});
+                });
+                landmark.Image = target_path;
+                res.status(201).send(landmark);
+            });
+        } else {
+            res.status(404);
+            console.log("There is no landmarkId given");
+        }
     });
 }
 
